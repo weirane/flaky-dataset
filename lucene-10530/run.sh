e742142d99ab3e5c09d9900da2b7e2f12d01ac85
Wang Ruochen <wrc@ruo-chen.wang>
2024-11-02 23:03:08 -0400

    lucene-10530

--- lucene-10530/run.sh
+++ lucene-10530/run.sh
@@ -9,5 +9,4 @@ cd code
 git checkout 0dad9ddae87beb888b9374f9fb5f223e5940e586
 
 # export PATH=/env/apache-maven-3.8.8/bin:$PATH
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 # export PATH=$JAVA_HOME/bin:$PATH
