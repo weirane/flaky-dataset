be546b91f04f2d6ac306a77260a75b98ebbc2522
Wang Ruochen <wrc@ruo-chen.wang>
2024-11-02 22:55:22 -0400

    template

---
+++ template/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+. ./env.sh
+
+cd "$(realpath "$(dirname "$0")")"
+git clone <https://github.com/apache/hadoop> code
+cd code
+git checkout <a53d62ab26e170a0338f93e228718da52e9196e4>
+
+# patch
+
+# compile
+mvn clean install -DskipTests -pl hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient -am
