35d15293d8025d9f2e496bb16991874d606746c0
Wang Ruochen <wrc@ruo-chen.wang>
2024-11-04 13:44:21 -0500

    include patch

--- mapreduce-6104/run.sh
+++ mapreduce-6104/run.sh
@@ -9,5 +9,6 @@ git clone https://github.com/apache/hadoop code
 cd code
 git checkout 034df0e2eb2824fb46a1e75b52d43d9914a04e56
 git apply ../1.patch
+git apply ../2.patch
 
 mvn -Dhttps.protocols=TLSv1.2 clean install -DskipTests -pl hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-hs -am
