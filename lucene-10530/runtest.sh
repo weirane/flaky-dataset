1449b715c291b9e4b69458e44f41666c417fda90
Wang Ruochen <wrc@ruo-chen.wang>
2024-12-09 22:46:55 -0500

    remove lucene

--- lucene-10530/runtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-cd "$(realpath "$(dirname "$0")")"/code
-
-. ../env.sh
-
-./gradlew test --tests TestTaxonomyFacetAssociations.testFloatAssociationRandom -Dtests.seed=4DFBA8209AC82EB2 -Dtests.slow=true -Dtests.locale=fr-VU -Dtests.timezone=Europe/Athens -Dtests.asserts=true -Dtests.file.encoding=UTF-8
